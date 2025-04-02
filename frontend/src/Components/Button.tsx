f53828e77fa0eab62c0c2c93ed0107d05f8be6eb
Turbobot <turbobot@vercel.com>
2025-04-02 19:04:57 +0530

    feat(create-turbo): apply official-starter transform

--- frontend/src/Components/Button.tsx
+++
@@ -1,8 +0,0 @@
-export const Button=({label,onClick}:{
-    label:string,
-    onClick:()=> void
-})=>{
-    return(
-        <button onClick={onClick} className=" m-6 rounded-xl p-2 md:p-4 text-lg md:text-xl text-yellow-100 font-mono hover:cursor-pointer bg-gradient-to-r  from-indigo-300 to-slate-600 hover:shadow-2xl ">{label}</button>
-    )
-}
